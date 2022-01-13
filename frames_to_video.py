9cf31811b02900f543bd872f1b3d1946eb349752
tgonzalez <mteresa290895@gmail.com>
2022-01-13 12:17:01 +0100

    remove all

--- frames_to_video.py
+++
@@ -1,8 +0,0 @@
-import ffmpeg
-
-(
-    ffmpeg
-    .input('/home/tgonzalez/Desktop/projects/airLUISA/data/montaje_video/*.jpg', pattern_type='glob', framerate=30)
-    .output('/home/tgonzalez/Desktop/projects/airLUISA/data/movie.mp4')
-    .run()
-)
